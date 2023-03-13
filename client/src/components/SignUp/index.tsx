9fec573cae1b26f775ccfa80f8db88852c9f6755
sai2yeshwanth <yashsai@gmail.com>
2023-03-13 11:15:37 +0530

    inprogress

---
+++ client/src/components/SignUp/index.tsx
@@ -0,0 +1,11 @@
+import { Box } from "@mui/material"
+import React from "react"
+
+const SignUpPage = () => {
+    return (
+        <Box sx={{width:'100%', height:'100%'}}>
+            hello world
+        </Box>
+    )
+}
+export default SignUpPage
